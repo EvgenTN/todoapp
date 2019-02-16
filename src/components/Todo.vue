e2b5173a962141ea6845f90ca5f72daac7138b63
egor066416 <066416@gmail.com>
2019-02-16 19:43:41 +0200

    view todo item

---
+++ src/components/Todo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="todo">
+    <input id="sss" type="checkbox" checked="isChecked">
+    <label for="sss">
+      <span>{{ description }} </span>
+      <span>{{ isChecked }}</span>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  props: {
+    isChecked: Boolean,
+    description: String
+  }
+};
+</script>
+<style lang="scss">
+
+</style>
