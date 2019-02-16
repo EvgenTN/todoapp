c018189bfe607b91c5d9db23a92307dab95d5eaa
evgentn <evgentitovn@gmail.com>
2019-02-16 19:50:39 +0200

    add_todolist

---
+++ src/components/Todos.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <h1>This is todo page</h1>
+    <h3>{{ todopr }}</h3>
+    <h3>{{ second }}</h3>
+    <!-- <h3>{{ todos[0].description }}</h3> -->
+    <!-- <div
+      v-for="todo in todos"
+      :key="todo.id"
+      >{{ todo.description }}</div> -->
+    <!-- <pre>{{ todos }}</pre> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todos',
+  props: {
+    todopr: String,
+    second: String,
+    todos: []
+  },
+  data: {
+
+  },
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
